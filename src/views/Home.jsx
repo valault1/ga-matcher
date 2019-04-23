43d7a3feff8c77a1ede213867bd817aa24bc35e0
Val Ault <valault1@gmail.com>
2019-04-23 11:57:55 -0500

    Merging

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,28 +1,6 @@
 import React from "react";
-import ReactPaginate from 'react-paginate';
-import CSVLink from 'react-csv';
-//import classnames from "classnames";
 
-// core components
-//import IndexNavbar from "components/Navbars/IndexNavbar.jsx";
-//import PageHeader from "components/PageHeader/PageHeader.jsx";
-//import Footer from "components/Footer/Footer.jsx";
-
-// sections for this page/view
-//import Basics from "views/IndexSections/old/Basics.jsx";
-//import Navbars from "views/IndexSections/old/Navbars.jsx";
-//import Tabs from "views/IndexSections/old/Tabs.jsx";
-//import Pagination from "views/IndexSections/old/Pagination.jsx";
-//import Notifications from "views/IndexSections/old/Notifications.jsx";
-//import Typography from "views/IndexSections/old/Typography.jsx";
-//import JavaScript from "views/IndexSections/old/JavaScript.jsx";
-//import NucleoIcons from "views/IndexSections/old/NucleoIcons.jsx";
-//import Signup from "views/IndexSections/old/Signup.jsx";
-//import Examples from "views/IndexSections/old/Examples.jsx";
-//import Download from "views/IndexSections/old/Download.jsx";
 import PrimaryNavBar from "components/Navbars/PrimaryNavBar.jsx";
-//import Courses from "views/IndexSections/Courses.jsx";
-//import Names from "views/IndexSections/Names.jsx";
 
 // reactstrap components
 import {
